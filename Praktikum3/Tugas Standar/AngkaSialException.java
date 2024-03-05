936fbc5c6c02bc107ec7d2c7c0a4b95c8a541db9
abyan setyaneva <127168084+abyansetya@users.noreply.github.com>
2024-03-05 14:26:06 +0700

    Create AngkaSialException.java

---
+++ Praktikum3/Tugas Standar/AngkaSialException.java
@@ -0,0 +1,9 @@
+//file : AngkaSialException.java 05/03/2024
+//nama : Abyan Setyaneva
+//NIM  : 24060122130058
+
+public class AngkaSialException extends Exception {
+    public AngkaSialException() {
+        super("jangan masukkan angka 13 karena angka sial!!!");
+    }
+}
