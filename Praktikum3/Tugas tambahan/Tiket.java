8af2d6dbf3dc6fc5112a9ea338fd6c2c56d46419
abyan setyaneva <127168084+abyansetya@users.noreply.github.com>
2024-03-06 22:34:59 +0700

    Create Tiket.java

---
+++ Praktikum3/Tugas tambahan/Tiket.java
@@ -0,0 +1,44 @@
+//file : Tiket.java 06/03/2024
+//Nama : Abyan Setyaneva
+//NIM  : 24060122130058
+//lab  : B2
+
+public class Tiket {
+    public static void pesanJumlahTiket(int jumlahTiket) {
+        // Asumsi jumlah tiket yang tersedia (boleh diganti)
+        int tiketTersedia = 100;
+
+        // Assertion untuk memastikan jumlah pemesanan lebih dari 0
+        assert jumlahTiket > 0 : "Jumlah pemesanan harus lebih dari 0";
+
+        // Assertion untuk memastikan jumlah pemesanan tidak melebihi tiket yang tersedia
+        assert jumlahTiket <= tiketTersedia : "Jumlah pemesanan melebihi tiket yang tersedia";
+
+        System.out.println("Pemesanan tiket sebanyak " + jumlahTiket + " berhasil.");
+    }
+
+    public static void isEWallet(int EWallet) {
+        // Memeriksa apakah index e-wallet yang dipilih valid
+        assert EWallet >= 1 && EWallet <= 4 : "Indeks e-wallet tidak valid";
+
+        // Assertion untuk memastikan e-wallet yang dipilih valid
+        assert !getEWallet(EWallet).isEmpty() : "E-wallet tidak valid";
+
+        System.out.println("Pemesanan tiket selesai menggunakan e-wallet " + getEWallet(EWallet));
+    }
+
+    private static String getEWallet(int index) {
+        switch (index) {
+            case 1:
+                return "OVO";
+            case 2:
+                return "GoPay";
+            case 3:
+                return "DANA";
+            case 4:
+                return "LinkAja";
+            default:
+                return "";
+        }
+    }
+}
