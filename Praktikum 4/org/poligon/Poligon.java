c6be5a64db2c4f6738f2447c7b700ed170b6eb99
abyan setyaneva <127168084+abyansetya@users.noreply.github.com>
2024-03-16 14:44:46 +0700

    Create Poligon.java

---
+++ Praktikum 4/org/poligon/Poligon.java
@@ -0,0 +1,14 @@
+// File : Poligon.java 16/03/2024
+// Nama : Abyan Setyaneva
+// NIM  : 24060122130058
+// Desk : representasi dasar dari objek poligon
+
+package org.poligon;
+
+public class Poligon{
+    protected int jumlahSisi;
+
+    public int getJumlahSisi(){
+        return this.jumlahSisi;
+    }
+}
