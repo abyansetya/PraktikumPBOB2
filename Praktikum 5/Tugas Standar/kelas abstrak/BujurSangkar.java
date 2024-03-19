39d6e2e11e6ce23dcd788ab2e37c7a6637f831f4
abyan setyaneva <127168084+abyansetya@users.noreply.github.com>
2024-03-19 17:53:24 +0700

    Create BujurSangkar.java

---
+++ Praktikum 5/Tugas Standar/kelas abstrak/BujurSangkar.java
@@ -0,0 +1,12 @@
+//File : BujurSangkar.java 19/03/2024
+//Nama : Abyan Setyaneva
+//NIM  : 24060122130058
+//desk : kelas yang membuat implementasi metode abstrak pada bangun datar
+
+public class BujurSangkar extends BangunDatar {
+    public double hitungLuas(double sisi){
+        luas = sisi*sisi;
+        return luas;
+    }
+    
+}
