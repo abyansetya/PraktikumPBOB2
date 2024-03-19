09e4e3f8816460a4cc6dc59e5b66bc6cc59dd61e
abyan setyaneva <127168084+abyansetya@users.noreply.github.com>
2024-03-19 18:46:14 +0700

    Create IArea.java

---
+++ Praktikum 5/Tugas Standar/kelas interface/IArea.java
@@ -0,0 +1,9 @@
+//File : IArea.java 19/03/2024
+//Nama : Abyan Setyaneva
+//NIM  : 24060122130058
+//desk : interface yang mendefinisikan kontrak bahwa setiap kelas yang membuat implementasi dari interface
+//       ini harus memiliki cara untuk menghitung luas
+
+public interface IArea {
+    public double hitungLuas();
+}
