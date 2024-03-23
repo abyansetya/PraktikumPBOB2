788c58f1dd0a2f93a250f44fd21ecfffef89c115
abyan setyaneva <127168084+abyansetya@users.noreply.github.com>
2024-03-23 20:30:18 +0700

    Update BukuAkademik.java

--- Praktikum 4/Tugas Tambahan/bk/bukuNonFiksi/BukuAkademik.java
+++ Praktikum 4/Tugas Tambahan/bk/bukuNonFiksi/BukuAkademik.java
@@ -4,8 +4,8 @@
 
 package bk.bukunonFiksi;
 
-public class bukuAkademik extends BukuNonFiksi{
-    public bukuAkademik(String judul, String penulis, String tahunTerbit, String subjek, int jumlahHalaman, double harga){
+public class BukuAkademik extends BukuNonFiksi{
+    public BukuAkademik(String judul, String penulis, String tahunTerbit, String subjek, int jumlahHalaman, double harga){
         super(judul, penulis, tahunTerbit, subjek, jumlahHalaman, harga);
     }
 }
