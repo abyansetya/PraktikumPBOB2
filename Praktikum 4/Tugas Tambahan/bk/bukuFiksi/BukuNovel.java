e49fe6e88c2166ec7ec0a4c62e024f77664033d8
abyan setyaneva <127168084+abyansetya@users.noreply.github.com>
2024-03-23 20:34:51 +0700

    Update BukuNovel.java

--- Praktikum 4/Tugas Tambahan/bk/bukuFiksi/BukuNovel.java
+++ Praktikum 4/Tugas Tambahan/bk/bukuFiksi/BukuNovel.java
@@ -1,7 +1,11 @@
+//File : BukuNovel.java
+//Nama : Abyan Setyaneva
+//File : 24060122130058
+
 package bk.bukuFiksi;
 
-public class bukuNovel extends BukuFiksi{
-    public bukuNovel(String judul, String penulis, String tahunTerbit, String genre, int jumlahHalaman, double harga){
+public class BukuNovel extends BukuFiksi{
+    public BukuNovel(String judul, String penulis, String tahunTerbit, String genre, int jumlahHalaman, double harga){
         super(judul, penulis, tahunTerbit, genre, jumlahHalaman, harga);
     }
 }
