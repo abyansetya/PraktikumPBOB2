cda65e44065b62bdbb6bf4949a4cd30dc175ce2d
abyan setyaneva <127168084+abyansetya@users.noreply.github.com>
2024-03-23 20:26:20 +0700

    Create BukuAkademik.java

---
+++ Praktikum 4/Tugas Tambahan/bk/bukuNonFiksi/BukuAkademik.java
@@ -0,0 +1,11 @@
+//File : BukuAkademik.java
+//Nama : Abyan setyaneva
+//NIM  : 24060122130058
+
+package bk.bukunonFiksi;
+
+public class bukuAkademik extends BukuNonFiksi{
+    public bukuAkademik(String judul, String penulis, String tahunTerbit, String subjek, int jumlahHalaman, double harga){
+        super(judul, penulis, tahunTerbit, subjek, jumlahHalaman, harga);
+    }
+}
