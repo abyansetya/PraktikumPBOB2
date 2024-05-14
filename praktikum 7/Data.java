bb2a4512efb16af4c1abe08155829dbcbe7ee64f
abyan setyaneva <127168084+abyansetya@users.noreply.github.com>
2024-05-14 18:36:15 +0700

    Create Data.java

---
+++ praktikum 7/Data.java
@@ -0,0 +1,13 @@
+//Nama : AByan Setyaneva
+//NIM  : 24060122130058
+
+public class Data<T> {
+    private T isi;
+    
+    public T getIsi(){
+        return this.isi;
+    }
+    public void setIsi(T isi){
+        this.isi = isi;
+    }
+}
