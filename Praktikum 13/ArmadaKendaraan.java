3b08d5b7876b78c35f3cb90e686c2d952e3efb24
abyan setyaneva <127168084+abyansetya@users.noreply.github.com>
2024-06-02 23:27:07 +0700

    Create ArmadaKendaraan.java

---
+++ Praktikum 13/ArmadaKendaraan.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ArmadaKendaraan<T extends vehicle>{
+    private List<T> armadakendaraan = new ArrayList<>();
+
+    public void tambahArmada(List<? extends T> armada){
+        for(int i = 0; i<=(armada.size()-1); i+=1) {
+    		armadakendaraan.add(armada.get(i));
+		}
+    }
+    public List<T> getAllArmada(){
+        return armadakendaraan;
+    }
+
+}
